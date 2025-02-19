ee82b8eb2e22eabe868c5796ca13c894fd5b75bf
med <simojarboue24@gmail.com>
2025-02-19 20:17:04 +0100

    added the logic to track emails

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask, jsonify, send_file, request
 from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import os
+import os, io
 import base64
 import smtplib
-import logging
+import logging, requests
 
 # # Load environment variables
 load_dotenv()
@@ -17,12 +17,18 @@ app = Flask(__name__)
 
 @app.route('/pixel', methods=["GET"])
 def tracking_pixel():
-    """Track email opens by logging the event and returning a 1x1 pixel GIF."""
-    logging.info("Email opened!") # print an event message that you received the signal that the email was opened
+    """Track email opens by logging the event and returning a 1x1 transparent GIF."""
+    logging.info("Email opened!")  # Log the event that the email was opened
     print("opened images")
+    print(f"the arguments in the img request is this ({request.args.get("email_id")})")
+    
+    # Create a transparent 1x1 GIF in memory
+    transparent_gif = b'GIF89a\x01\x01\x80\x00\x00\x00\x00\x00\x00\xFF\xFF\xFF,\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00;'
+    # here do the changes you want like changing the variable from false to true
+    ############################################################################
     return send_file(
-        "/app/app/mjarboua.jpg",
-        mimetype='image/jpg',
+        io.BytesIO(transparent_gif),
+        mimetype='image/gif',
         as_attachment=False
     )
 
@@ -34,6 +40,7 @@ def send_email(to_email):
         return 0
     else:
         print("file doesnt exist")
+
     print(os.getenv('PIXEL_URL')) # store your pixel/GIF/image url in a ENV variable
     msg = MIMEMultipart()
     msg['From'] = os.getenv('EMAIL_USER') # your email that you which to use in your campaign
@@ -47,7 +54,7 @@ def send_email(to_email):
     <body>
             <p>Hello!</p>
             <p>This is a test email.</p>
-            <img src="http://157.175.44.139:8080/pixel" width="200" height="200" />
+            <img src="http://157.175.44.139:8080/pixel?email_id=somth3243asd3qer" width="1" height="1" />
     </body>
  </html>
     """
