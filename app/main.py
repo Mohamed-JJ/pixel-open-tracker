ee35502f04a44f62a66ed58c0ae0e2587e3c3d90
med <simojarboue24@gmail.com>
2025-02-19 21:24:44 +0100

    changing somethings, and generating a uuid for the pixel to use it for tracking later on

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os, io
-import base64
+import base64, uuid
 import smtplib
 import logging, requests
 
@@ -20,43 +20,54 @@ def tracking_pixel():
     """Track email opens by logging the event and returning a 1x1 transparent GIF."""
     logging.info("Email opened!")  # Log the event that the email was opened
     print("opened images")
-    email_id = request.args.get("email_id")
-    logging.info(f"the arguments in the img request is this :{email_id}")
+    pixel_id = request.args.get("pixel_id")
+    logging.info(f"the arguments in the img request is this :{pixel_id}")
     # Create a transparent 1x1 GIF in memory
     transparent_gif = b'GIF89a\x01\x01\x80\x00\x00\x00\x00\x00\x00\xFF\xFF\xFF,\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00;'
     # here do the changes you want like changing the variable from false to true
-
+    
     ############################################################################
     return send_file(
         # (image path if you want to use images)
-        io.BytesIO(transparent_gif), # can be removed (making the n¡binary in file format to work eith the send_file function)
+        io.BytesIO(transparent_gif), # can be removed (making the binary in file format to work eith the send_file function)
         mimetype='image/gif',
         as_attachment=False
     )
 
+def check_image(image_path):
+    if (os.path.exists(path=image_path)):
+        return 1
+    else:
+        return 0
+
+def generate_random_uuid():
+    return str(uuid.uuid4())
+
 def send_email(to_email):
     """Send an email with a tracking pixel."""
-    # a picture (optional), can be ommitted and use a GIF 
-    if (os.path.exists(path="./app/mjarboua.jpg")):
-        print("file exists")
-        return 0
-    else:
-        print("file doesnt exist")
+    # a picture (optional), can be ommitted and use a GIF
+    # if (check_image("<the file which you want to use>") == 0):
+    #     print("file exists")
+    #     return 0
+    # else:
+    #     print("file doesnt exist")
 
-    print(os.getenv('PIXEL_URL')) # store your pixel/GIF/image url in a ENV variable
+
     msg = MIMEMultipart()
     msg['From'] = os.getenv('EMAIL_USER') # your email that you which to use in your campaign
     msg['To'] = to_email # the receiver
     msg['Subject'] = 'Test Email with Tracking'
-    # change the picture height and width to what you prefer (1x1 pixel)
     # change the url of the picture to where your picture is hosted, preferrably a cloud solution like aws or google cloud run
     # the /pixel is the route from which where we serve the picture from
+
+    # generate a random id to save in the database and send in the html body in the pixel_id
+    random_id = generate_random_uuid()
     html = f"""
     <html>
     <body>
             <p>Hello!</p>
             <p>This is a test email.</p>
-            <img src="http://157.175.44.139:8080/pixel?email_id={"somth3243asd3qer"}" width="1" height="1" />
+            <img src="http://157.175.44.139:8080/pixel?pixel_id={random_id}" width="1" height="1" />
     </body>
  </html>
     """
